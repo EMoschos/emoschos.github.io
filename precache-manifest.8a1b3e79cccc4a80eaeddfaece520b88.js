be17f6e973e53846bbacd2bf5486406e2fd71ecd
Eleftherios Moschos <tmoschos@bigpond.com>
2020-10-03 23:19:15 +0930

    Updates

---
+++ precache-manifest.8a1b3e79cccc4a80eaeddfaece520b88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d89cb7db11a5c9d4dd512755f89a59e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74e6677db25a0c271e9f",
+    "url": "/static/js/2.fae911b9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fae911b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f02dab5679944f83a381",
+    "url": "/static/js/main.e82f0928.chunk.js"
+  },
+  {
+    "revision": "e37465518915254c8288",
+    "url": "/static/js/runtime-main.f99caf9d.js"
+  }
+]);
